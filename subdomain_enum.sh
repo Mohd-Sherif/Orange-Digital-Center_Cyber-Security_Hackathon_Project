ffd639be062b898395dfbaedd4e704131b913451
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-04-15 14:59:59 +0200

    Scanning Subdomains

--- subdomain_enum.sh
+++ subdomain_enum.sh
@@ -7,3 +7,12 @@ if [ $# -eq 0 ]
 then
         echo "How to use: ./subdomain_enum.sh <DOMAIN>"
         echo "EX: ./subdomain_enum.sh yahoo.com"
+else
+
+	echo "++++++ Scanning Subdomains ++++++"
+	if [ -a subdomains.txt ]
+	then
+       		rm subdomains.txt
+	fi
+	subfinder -d $1 -o subdomains.txt
+
