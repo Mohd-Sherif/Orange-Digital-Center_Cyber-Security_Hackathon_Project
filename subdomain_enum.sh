bc448be4cdd2a1f5e11720959a9e7a61a8c1a72f
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-04-15 14:59:30 +0200

    How to use guide

--- subdomain_enum.sh
+++ subdomain_enum.sh
@@ -3,3 +3,7 @@
 #This tool developed by Mohamed Sherif
 #ODC Hackathon
 
+if [ $# -eq 0 ]
+then
+        echo "How to use: ./subdomain_enum.sh <DOMAIN>"
+        echo "EX: ./subdomain_enum.sh yahoo.com"
